822e95090a2d38fc59db8b60aff827bbbf60579a
JQ Chan <vikers28@icloud.com>
2018-09-09 01:59:29 +0800

    地图sdk加入

---
+++ src/libs/config.js
@@ -0,0 +1,7 @@
+var config = {
+  amapKey: 'c9607eda9f088947d97e836c9cfd8c17',
+  baiduAk: 'u5nxtZiYUE590ES0xVomx3ZZa04cAuv2',
+  qqmapKey: 'GHQBZ-2CRCG-2OYQS-IQJ75-6VVGV-CMBF4'
+}
+
+module.exports.Config = config;
